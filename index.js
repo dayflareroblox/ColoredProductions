23b1c3dfe0afed22ec21ee7818cdbc55ba67f5a5
websitemanagement <websitemanagement123@gmail.com>
2019-03-10 12:54:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: false});
 
+const ownerID = '481171799204429834';
+
 //---------------------------------------------------------------\\//---------------------------------------------------------------\\
 
 function changing_status() {
@@ -85,7 +87,24 @@ if(cmd === `${prefix}pay`){
     
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
 
-   
+ if(cmd === `${prefix}servers`){
+
+   if (message.author.id !== ownerID) return message.channel.send("You are not authorized to use this command.");
+   let string = '';
+
+   client.guilds.forEach(guild => {
+       string += '***Server Name:*** ' + guild.name + '\n' + '***Server ID:***` ' + guild.id + ' ` ' + '\n\n';
+
+   })
+
+   let botembed = new Discord.RichEmbed()
+       .setColor("#000FF")
+       .addField("Bot is On ", string)
+       .setTimestamp()
+       .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL);
+   message.channel.send(botembed);
+
+  }   
 
 });
 
