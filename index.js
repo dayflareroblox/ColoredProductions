439573710162322aa43e29454a4ad6a75fb711ba
websitemanagement <websitemanagement123@gmail.com>
2019-03-10 13:12:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ bot.on("message", async message => {
      .addField("**Owner.**", "Message Color3fromHSL if you urgently need him.")
      .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
      .setTimestamp(); 
-                   
+
         message.channel.send(Embed)
     }  
     
@@ -108,8 +108,36 @@ if(cmd === `${prefix}pay`){
        .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL);
    message.channel.send(botembed);
 
+  }  
+  
+ //---------------------------------------------------------------\\//---------------------------------------------------------------\\
+
+ if(cmd === `${prefix}kickbot`){
+
+   if (message.author.id !== ownerID) return message.channel.send("You are not authorized to use this command.");
+
+   var error17 = new Discord.RichEmbed()
+       .setDescription('**Please enter a valid server ID.**')
+       .setColor(0xff0000)
+
+   var error18 = new Discord.RichEmbed()
+       .setDescription('**You cannot kick the bot from this server!**')
+       .setColor(0xff0000)
+
+
+   if (isNaN(args[0])) return message.channel.send(error17).then(msg => {
+       msg.delete(9000)
+   });
+
+   //If tried kick bot from a main server (like for emote provider) will return error18
+   if (args[0] !== 373950345153609729 || 481663437705838602) return message.channel.send(error18).then(msg => {
+       msg.delete(9000)
+   });
+
+   client.guilds.get(args[0]).leave();
+   message.channel.send(`**Bot was been removed from server id [${args[0]}]**`)
+
   }   
 
 });
-
 bot.login(process.env.BOT_TOKEN);
